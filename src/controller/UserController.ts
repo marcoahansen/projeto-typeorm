35dd290ab8f634f734516981a43f746a3757f547
Marco Aurelio Hansen de Oliveira <ssnpett18@firjan.com.br>
2026-04-30 21:43:00 -0300

    ajuste na estrutura do update user

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -41,40 +41,34 @@ export class UserController {
     try {
       const { firstName, lastName, email, phone, password } = req.body;
       const id = req.user_id;
-      if (id && isNaN(id)) {
-        throw new BadRequestError("ID inválido");
+      const tempData = this.userRepository.create({
+        firstName,
+        lastName,
+        email,
+        phone,
+        password,
+      });
+      const errors = await validate(tempData, { skipMissingProperties: true });
+      if (errors.length > 0) {
+        throw new BadRequestError("Falha de validação", formatErrors(errors));
       }
       const user = await this.userRepository.findOneBy({ id });
-      if (!user) {
-        throw new NotFoundError("Usuário não encontrado");
-      }
+      if (!user) throw new NotFoundError("Usuário não encontrado");
       if (email && email !== user.email) {
         const exists = await this.userRepository.findOneBy({ email });
-        if (exists) {
-          throw new BadRequestError(
-            "Este e-mail já está em uso por outro usuário!"
-          );
-        }
+        if (exists) throw new BadRequestError("Este e-mail já está em uso!");
         user.email = email;
       }
       user.firstName = firstName ?? user.firstName;
       user.lastName = lastName ?? user.lastName;
       user.phone = phone ?? user.phone;
       if (password) {
-        user.password = password;
-      }
-      const errors = await validate(user, { skipMissingProperties: true });
-      if (errors.length > 0) {
-        const formattedErrors = formatErrors(errors);
-        throw new BadRequestError("Falha de validação", formattedErrors);
-      }
-      if (password) {
         user.password = await bcrypt.hash(password, 10);
       }
       await this.userRepository.save(user);
       const { password: _, ...userPublic } = user;
       return res.status(200).json(userPublic);
-    } catch (error: unknown) {
+    } catch (error) {
       next(error);
     }
   };
